41e02504f066d31216819cf7c6366ddf4b416207
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-11-14 16:16:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+@use"./style/general.scss" as *;
+</style>
