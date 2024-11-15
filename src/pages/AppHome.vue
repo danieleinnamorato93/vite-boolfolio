81ba8be61543381c70cfea6d0190d5b8de0bc915
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-11-15 16:01:02 +0100

    routes created

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  name: "AppHome",
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    getProjects() {
+
+  },
+  created() {
+
+  },
+};
+}
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1>App Home:</h1>
+        </div>
+
+        <div class="col-12 text-center">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+            voluptates. Obcaecati, ratione eaque. Aspernatur, sint officia. Aut
+            qui maiores, tempora id ipsa tenetur asperiores possimus ratione
+            esse, nulla fugiat facilis! Ducimus eaque rerum officiis, expedita
+            ex, consequuntur ea incidunt harum sit adipisci laborum, molestias
+            alias vel aperiam error nisi sunt sint? Quis rem necessitatibus
+            ipsum quasi itaque, doloremque tempora? Quam. Aliquam veniam facere
+            modi nemo, facilis dignissimos vel officia eligendi ratione quae sit
+            adipisci laborum, repellat reprehenderit totam sint magnam. Quis
+            accusantium ex cum ducimus id nesciunt esse blanditiis sunt.
+          </p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+h1 {
+  color: black;
+}
+a {
+  color: red;
+}
+</style>
